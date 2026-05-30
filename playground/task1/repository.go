3488e2422ae0989161fe090f0b95806d15c46d9a
Zvoook <rezviipersik@gmail.com>
2026-05-30 13:06:28 +0300

    Update in-memory task repository practice

--- playground/task1/repository.go
+++ playground/task1/repository.go
@@ -9,6 +9,29 @@ type TaskRepository interface {
 	Delete(id uint32) error
 }
 
-type InMemoryTaskRepository struct {
+type InMemoryTaskRepository struct{
 	tasks map[uint32]Task
+	nextId uint32
 }
+
+func (r* InMemoryTaskRepository) getId() uint32 {
+	r.nextId++
+	return r.nextId
+}
+
+func (r* InMemoryTaskRepository) Save (t *Task) error {
+	id := r.getId()
+	t.id = id
+	r.tasks[id] = *t
+	return nil
+}
+
+func (r* InMemoryTaskRepository) FindById (id uint32) (Task, error) {
+	task, contains := r.tasks[id]
+	if contains {
+		return task, nil
+	}
+	return Task{}, ErrTaskNotFound
+}
+
+func (r* InMemoryTaskRepository)
